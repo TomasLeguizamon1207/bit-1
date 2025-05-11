eced68074e1cc6610c558ba7274d79f0453b28f0
Tomás Leguizamón <tomasleguizamon120708@gmail.com>
2025-05-10 23:22:41 -0500

    Se termina la implementacion de JavaScript en el contactenos, para la animacion de cada espacio y para el enviado del mensaje al correo por medio de EmailJS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 "use strict";
+const inputs = document.querySelectorAll (".input");
+
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc(){
+    let parent = this.parentNode;
+    if(this.value == ""){
+    parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
+
+
+  emailjs.init('0JPY9L_shz4WsCRIJ'); // Tu Public Key
+
+  const btn = document.getElementById('button');
+
+  document.getElementById('form')
+    .addEventListener('submit', function(event) {
+      event.preventDefault();
+      btn.value = 'Enviando...';
+
+      const serviceID = 'default_service';
+      const templateID = 'template_r8o6peo'; // Asegúrate que esté bien
+
+      emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+          btn.value = 'Enviar';
+          alert('Mensaje enviado correctamente.');
+        }, (err) => {
+          btn.value = 'Enviar';
+          alert('Error al enviar: ' + JSON.stringify(err));
+        });
+    });
